9b29c623ff70ecee7cc022edbfe49b13cf33daea
akshatasaha <akshatasaha001@gmail.com>
2022-11-16 17:05:16 +0530

    adding java files

---
+++ javaday1/DataMember.java
@@ -0,0 +1,17 @@
+class DataMember
+{
+//datamembers
+static int i =78;
+static boolean b= true;
+static char c= 'k';
+public static void main(String[] args)
+{
+System.out.println("start");
+System.out.println(i);
+System.out.println(b);
+System.out.println(c);
+System.out.println("end");
+
+}
+
+}
---
+++ javaday1/DataType.java
@@ -0,0 +1,27 @@
+class DataType
+{
+//varaibles declared in class are called data members
+//to call the data members use keyword "static" eg - static int a;
+public static void main(String[] args)
+{
+//varaibles declared in the method are called local variable
+Byte B=123;
+System.out.println(B);
+int i =45689;
+System.out.println(i);
+int s =(short)7997;
+System.out.println(s);
+long l=679800;
+System.out.println(l);
+double d=6878.79890000;
+System.out.println(d);
+float f=8799.7989f;
+System.out.println(f);
+char c= 't';
+System.out.println(c);
+boolean b = false;
+System.out.println(b);
+String a = "Akshata";
+System.out.println(a);
+}
+}
